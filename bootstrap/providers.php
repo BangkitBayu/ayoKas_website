b29bf9ba93f87abf7f6b08549ad244cefdd711ee
BangkitBayu <martinusmartinussaja@gmail.com>
2025-11-08 12:18:19 +0700

    chore: Setup laravel debugbar to helping debug

--- bootstrap/providers.php
+++ bootstrap/providers.php
@@ -1,6 +1,9 @@
 <?php
 
+use Barryvdh\Debugbar\ServiceProvider;
+
 return [
     App\Providers\AppServiceProvider::class,
     App\Providers\VoltServiceProvider::class,
+    ServiceProvider::class
 ];
